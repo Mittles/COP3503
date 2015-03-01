11ab4956514208d686f54afd9f0a2896698e8e24
Mittles <crispy@nupson.net>
2015-03-01 12:03:07 -0500

    added some things

--- build-linux.sh
+++ build-linux.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 JOPTS=-j5
-BDIR=$(pwd)
+BDIR=$(pwd)/build
 
-mkdir -p $BDIR/bin
-mkdir -p $BDIR/lib
+mkdir -p $BDIR
 
 mkdir -p ./src/SDL/build
 cd ./src/SDL/build
-../configure --prefix=$BDIR --bindir=$BDIR/bin --libdir=$BDIR/lib
-make $JOPTS && make install-lib
+../configure --prefix=$BDIR 
+make $JOPTS && make install-lib && make install-hdrs
+
+GFLAGS=$(sdl2-config --cflags --libs)
+
+cd ../../
+
+
