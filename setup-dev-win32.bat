5d07a736189b347a2be6eb87605fd025c39253e3
Mittles <crispy@nupson.net>
2015-03-10 16:41:22 -0400

    a bit more sane ordering

--- setup-dev-win32.bat
+++ setup-dev-win32.bat
@@ -3,7 +3,7 @@ ECHO WAIT FOR WINDOW TO CLOSE
 ECHO DOWNLOADING CODEBLOCKS
 ECHO IF YOU HAVE OTHER VERSIONS OF CODEBLOCKS INSTALLED PLEASE REMOVE THEM NOW
 powershell -ExecutionPolicy Unrestricted -File setup-dev.ps1
-codeblocks.exe
 MKDIR bin
 MKDIR bin\Debug
 COPY lib\SDL2\bin\SDL2.dll bin\Debug
+codeblocks.exe
