151937cea44c0150b14b68b88c3fcef5080249ee
rtrumpet <fabienrosemond@gmail.com>
2015-04-07 17:36:18 -0400

    World and Territory Class

---
+++ include/Territory.h
@@ -0,0 +1,14 @@
+#ifndef TERRITORY_H
+#define TERRITORY_H
+
+
+class Territory
+{
+    public:
+        Territory();
+        virtual ~Territory();
+    protected:
+    private:
+};
+
+#endif // TERRITORY_H
