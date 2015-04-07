0b32be93ec0c5a0f0a05c2cc87c99dfe471f8e0a
gcamp95 <garretttcamp@ymail.com>
2015-04-07 18:42:49 -0400

    moved DiceRoll.h to include file

---
+++ include/DiceRoll.h
@@ -0,0 +1,16 @@
+#ifndef DICEROLL_H
+#define DICEROLL_H
+
+#include "DiceRoll.h"
+#include <cstdlib>
+#include <ctime>
+
+class DiceRoll
+{
+    public:
+        diceRoll(int seed);
+    protected:
+    private:
+};
+
+#endif // DICEROLL_H
