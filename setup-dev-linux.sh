580abceb4f0a885f4dbfa10ec87c026d3ae84808
Mittles <crispy@nupson.net>
2015-04-08 09:52:32 -0400

    oops

--- setup-dev-linux.sh
+++ setup-dev-linux.sh
@@ -9,6 +9,5 @@ cd ./src/SDL/build
 ../configure --prefix=$BDIR/SDL2
 make $JOPTS && make install-lib && make install-hdrs && make install-bin
 cd ../../SDL2_image
-./autogen.sh
 ./configure --prefix=$BDIR/SDL2
 make $JOPTS all && make install
