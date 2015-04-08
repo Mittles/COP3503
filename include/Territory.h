8fda9e60fdbb98475ec2119f0b8773c9de787a36
Doug0855 <doug0855@gmail.com>
2015-04-08 17:05:49 -0400

    Territory
    nickels

--- include/Territory.h
+++ include/Territory.h
@@ -1,14 +1,21 @@
 #ifndef TERRITORY_H
 #define TERRITORY_H
+#include <iostream>
+#include <string>
+#include <vector>
 
+using namespace std;
 
 class Territory
 {
     public:
-        Territory();
+        Territory(int num, string name, int borderNum, string borders[]);
         virtual ~Territory();
     protected:
     private:
+        int num;
+        string name;
+        vector<string> borders;
 };
 
 #endif // TERRITORY_H
--- src/Territory.cpp
+++ src/Territory.cpp
@@ -1,11 +1,13 @@
-/*#include "Territory.h"
+#include "Territory.h"
 
-Territory::Territory()
-{
-    //ctor
+Territory::Territory(int num, string name, int borderNum, string borders_array[]) {
+	this->num = num;
+	this->name = name;
+
+	for (int i=0; i<borderNum; i++) {
+        borders.push_back(borders_array[i]);
+	}
 }
 
-Territory::~Territory()
-{
-    //dtor
-}*/
+Territory::~Territory() {
+}
