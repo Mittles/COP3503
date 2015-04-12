68a4a20425e09923cd6da8780875cc02137b831a
Doug0855 <doug0855@gmail.com>
2015-04-11 20:44:43 -0400

    Debugged World, is compiling now

--- include/World.h
+++ include/World.h
@@ -1,28 +1,28 @@
-//#ifndef WORLD_H
-//#define WORLD_H
-//#include "Territory.h"
-//#include <string>
-//#include <vector>
-//#include <iostream>
-//
-//
-//using namespace std;
-//
-//class World
-//{
-//    public:
-//        World(Territory north_america_array[], Territory south_america_array[],
-//              Territory europe_array[], Territory africa_array[], Territory asia_array[], Territory australia_array[]);
-//        virtual ~World();
-//    protected:
-//    private:
-//        vector<Territory> north_america;
-//        vector<Territory> south_america;
-//        vector<Territory> europe;
-//        vector<Territory> africa;
-//        vector<Territory> asia;
-//        vector<Territory> australia;
-//        vector<vector<Territory>> world;
-//};
-//
-//#endif // WORLD_H
+#ifndef WORLD_H
+#define WORLD_H
+#include "Territory.h"
+#include <string>
+#include <vector>
+#include <iostream>
+
+
+using namespace std;
+
+class World
+{
+    public:
+        World(Territory* north_america_array[], Territory* south_america_array[],
+              Territory* europe_array[], Territory* africa_array[], Territory* asia_array[], Territory* australia_array[]);
+        virtual ~World();
+    protected:
+    private:
+        vector<Territory*> north_america;
+        vector<Territory*> south_america;
+        vector<Territory*> europe;
+        vector<Territory*> africa;
+        vector<Territory*> asia;
+        vector<Territory*> australia;
+        vector<vector<Territory*> > world;
+};
+
+#endif // WORLD_H
--- src/World.cpp
+++ src/World.cpp
@@ -1,41 +1,41 @@
-//#include "World.h"
-//
-//World::World(Territory north_america_array[], Territory south_america_array[],
-//              Territory europe_array[], Territory africa_array[], Territory asia_array[], Territory australia_array[])
-//{
-//    for (int i=0; i<9, i++) {
-//        north_america.push_back(north_america_array[i]);
-//    }
-//
-//    for (int i=0; i<4, i++) {
-//        south_america.push_back(south_america_array[i]);
-//    }
-//
-//    for (int i=0; i<7, i++) {
-//        europe.push_back(europe_array[i]);
-//    }
-//
-//    for (int i=0; i<6, i++) {
-//        africa.push_back(africa_array[i]);
-//    }
-//
-//    for (int i=0; i<12, i++) {
-//        asia.push_back(asia_array[i]);
-//    }
-//
-//    for (int i=0; i<4, i++) {
-//        australia.push_back(australia_array[i]);
-//    }
-//
-//    world.push_back(north_america);
-//    world.push_back(south_america);
-//    world.push_back(europe);
-//    world.push_back(africa);
-//    world.push_back(asia);
-//    world.push_back(australia);
-//}
-//
-//World::~World()
-//{
-//
-//}
+#include "World.h"
+
+World::World(Territory* north_america_array[], Territory* south_america_array[],
+              Territory* europe_array[], Territory* africa_array[], Territory* asia_array[], Territory* australia_array[])
+{
+    for (int i=0; i<9; i++) {
+        north_america.push_back(north_america_array[i]);
+    }
+
+    for (int i=0; i<4; i++) {
+        south_america.push_back(south_america_array[i]);
+    }
+
+    for (int i=0; i<7; i++) {
+        europe.push_back(europe_array[i]);
+    }
+
+    for (int i=0; i<6; i++) {
+        africa.push_back(africa_array[i]);
+    }
+
+    for (int i=0; i<12; i++) {
+        asia.push_back(asia_array[i]);
+    }
+
+    for (int i=0; i<4; i++) {
+        australia.push_back(australia_array[i]);
+    }
+
+    world.push_back(north_america);
+    world.push_back(south_america);
+    world.push_back(europe);
+    world.push_back(africa);
+    world.push_back(asia);
+    world.push_back(australia);
+}
+
+World::~World()
+{
+
+}
