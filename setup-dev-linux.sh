97e662c563556e189fa7b8aa1c98df6254411c01
Mittles <crispy@nupson.net>
2015-04-14 17:00:55 -0400

    ttf added

--- setup-dev-linux.sh
+++ setup-dev-linux.sh
@@ -8,7 +8,19 @@ mkdir -p ./src/SDL/build
 cd ./src/SDL/build
 ../configure --prefix=$BDIR/SDL2
 make $JOPTS && make install-lib && make install-hdrs && make install-bin
-cd ../../SDL2_image
+
+cd ../../
+
+echo HEY THERE (pwd)
+
+tar -xzf ./SDL2_image.tar.gz
+tar -xzf ./SDL2_ttf.tar.gz
+
+cd ./SDL2_image
+./configure --prefix=$BDIR/SDL2
+make $JOPTS all && make install
+
+cd ../SDL2_ttf
 ./configure --prefix=$BDIR/SDL2
 make $JOPTS all && make install
 
