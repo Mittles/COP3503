76af4393e63d1eb3321d7b1b992f283eccf58239
Doug0855 <doug0855@gmail.com>
2015-04-15 09:10:18 -0400

    re added layout

--- include/Card.h
+++ include/Card.h
@@ -10,19 +10,16 @@ class Card
     public:
         //constructor automatically sets cardOwner and cardValue both to 0 to be changed later in the Deck class
         Card();
-        void setOwner(int o);
-        int getOwner();
-        void setValue(int v);
-        int getValue();
-        ~Card();
 
-    protected:
-    private:
         //indicates which player owns the card
         int cardOwner;
 
         //indicates how many stars and therefore troops the card is worth
         int cardValue;
+        ~Card();
+
+    protected:
+    private:
 };
 
 #endif // CARD_H
--- src/Card.cpp
+++ src/Card.cpp
@@ -8,21 +8,6 @@ Card::Card(){
     cardValue = 0;
 }
 
-void Card::setOwner(int o){
-    cardOwner = o;
- }
-
-int Card::getOwner(){
-    return cardOwner;
- }
-
-void Card::setValue(int v){
-    cardValue = v;
- }
-
-int Card::getValue(){
-    return cardValue;
- }
 
 Card::~Card(){
 }
