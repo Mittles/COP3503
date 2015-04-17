1649348e2a469d834b2ee2d5fed03759323b9125
Mittles <crispy@nupson.net>
2015-04-17 17:30:07 -0400

    final presentation scripts

--- setup-final-linux.sh
+++ setup-final-linux.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 JOPTS=-j5
-BDIR=$(pwd)/build
+BDIR=$(pwd)
+GNAME=game.run
 
-mkdir -p $BDIR
+tar xf ubuntu-14.04_amd64_lib.tar
 
-mkdir -p ./src/SDL/build
-cd ./src/SDL/build
-../configure --prefix=$BDIR 
-make $JOPTS && make install-lib && make install-hdrs && make install-bin
+g++ -Wall -g -Iinclude/SDL2 -Iinclude -c $BDIR/src/Attack.cpp -o Attack.o
+g++ -Wall -g -Iinclude/SDL2 -Iinclude -c $BDIR/src/Card.cpp -o Card.o
+g++ -Wall -g -Iinclude/SDL2 -Iinclude -c $BDIR/src/Deck.cpp -o Deck.o
+g++ -Wall -g -Iinclude/SDL2 -Iinclude -c $BDIR/src/DiceRoll.cpp -o DiceRoll.o
+g++ -Wall -g -Iinclude/SDL2 -Iinclude -c $BDIR/src/Game.cpp -o Game.o
+g++ -Wall -g -Iinclude/SDL2 -Iinclude -c $BDIR/src/main.cpp -o main.o
+g++ -Wall -g -Iinclude/SDL2 -Iinclude -c $BDIR/src/MoveTroops.cpp -o MoveTroops.o
+g++ -Wall -g -Iinclude/SDL2 -Iinclude -c $BDIR/src/Player.cpp -o Player.o
+g++ -Wall -g -Iinclude/SDL2 -Iinclude -c $BDIR/src/Territory.cpp -o Territory.o
+g++ -Wall -g -Iinclude/SDL2 -Iinclude -c $BDIR/src/World.cpp -o World.o
 
-GFLAGS=$(sdl2-config --cflags --libs)
+g++ -Llib -o $GNAME Attack.o Card.o Deck.o DiceRoll.o Game.o main.o MoveTroops.o Player.o Territory.o World.o -lSDL2main -lSDL2 -lSDL2_image -lSDL2_ttf
 
-cd ../../
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.:$BDIR/lib
 
-
+./$GNAME
